2af8e1274618236f8f5576e0fa262a4f21994bc9
pkoptilin <pkoptilin@modeln.com>
2021-04-23 11:25:17 +0300

    dynamic rss

--- src/components/rss/RSSFeed.js
+++ src/components/rss/RSSFeed.js
@@ -1,8 +1,24 @@
-import * as React from "react"
 import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
+import React, { useState, useEffect } from "react"
+// import { parse } from 'fast-xml-parser';
 
 const RssPage = () => {
+
+  const rssUrl = `https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fnews.ycombinator.com%2Frss`;
+  const [newsTitle, setNewsTitle] = useState(0)
+  const [newsItems, setNewsItems] = useState(1)
+  useEffect(() => {
+    fetch(rssUrl)
+    // .then( textResponse => parse(textResponse) )
+    .then(response =>  response.json() ) 
+    .then( resultData => {
+      setNewsTitle(resultData.feed.title);
+      setNewsItems(resultData.items)
+    })
+    .catch((error) => { console.log(error);})
+
+  }, [])
 
   const data = useStaticQuery(graphql`
   query RssQuery {
@@ -24,17 +40,27 @@ const RssPage = () => {
   return (
 
         <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
-            <h1>News</h1>
+            
+            <h1>{newsTitle}</h1>
             <ul>
-            {edges.map((data, index) => {
+            {newsItems.map && newsItems.map((data, index) => {
+            return (
+              <li>
+                <Link to={data.link}>{data.title}</Link>
+              </li>
+              )
+            })}
+
+            {/* {edges.map((data, index) => {
             return( 
                 <li>
                     <Link to={data.node.link}>{data.node.title}</Link>
                 </li>
                 )
-            })}
+            })} */}
 
             </ul>
+
         </div>
 
   )}
